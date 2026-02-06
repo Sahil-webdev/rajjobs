a048f1d34e27922369ceef8c5e51198b883a90bc
Sahil-webdev <sahilshh777@gmail.com>
2026-02-06 13:20:27 +0530

    Remove AuthProvider from layout.tsx

--- web-frontend/src/app/layout.tsx
+++ web-frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import Footer from "@/components/footer";
-import { AuthProvider } from "@/contexts/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,11 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <Navbar />
-          <main className="bg-white min-h-screen">{children}</main>
-          <Footer />
-        </AuthProvider>
+        <Navbar />
+        <main className="bg-white min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
